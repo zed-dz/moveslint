915a4bdf8f9d083f55a0e63d9c04a83a4970ab39
Zed-M <fsociety.2017@protonmail.com>
2019-08-04 12:56:14 +0100

    adding babel-eslint

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const configs = {
 
   console.log('.eslintrc.json successfully created <3 !')
   shell.exec(
-    'yarn add -D prettier babel-eslint@10.0.1 eslint-config-airbnb eslint-config-prettier eslint-plugin-babel eslint-config-node eslint-plugin-flowtype eslint-plugin-html eslint-plugin-prettier babel-eslint eslint-plugin-react-hooks eslint-plugin-node eslint eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-config-google eslint-config-kentcdodds eslint-config-xo eslint-plugin-eslint-comments eslint-plugin-json eslint-plugin-optimize-regex eslint-plugin-promise eslint-plugin-unicorn babel-plugin-module-resolver babel-plugin-react-require jest webpack eslint-import-resolver-webpack eslint-import-resolver-babel-module && yarn audit'
+    'yarn add -D prettier babel-eslint@10.0.1 eslint-config-airbnb eslint-config-prettier eslint-plugin-babel eslint-config-node eslint-plugin-flowtype eslint-plugin-html eslint-plugin-prettier eslint-plugin-react-hooks eslint-plugin-node eslint eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-config-google eslint-config-kentcdodds eslint-config-xo eslint-plugin-eslint-comments eslint-plugin-json eslint-plugin-optimize-regex eslint-plugin-promise eslint-plugin-unicorn babel-plugin-module-resolver babel-plugin-react-require webpack eslint-import-resolver-webpack eslint-import-resolver-babel-module && yarn audit'
   )
   console.log('thank you for installing eslint node modules <3!')
 })()
