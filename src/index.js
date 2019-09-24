ea2f05316b6308310cfcb36c8a8d9c0aac4cb8aa
Zed-M <fsociety.2017@protonmail.com>
2019-09-24 10:20:40 +0100

    new version of eslint v6

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const configs = {
 
   console.log('.eslintrc.json successfully created <3 !')
   shell.exec(
-    'yarn add -D eslint@5.16.0 webpack@4.29.6 babel-eslint@10.0.1 prettier eslint-config-airbnb eslint-config-prettier eslint-plugin-babel eslint-config-node eslint-plugin-flowtype eslint-plugin-html eslint-plugin-prettier eslint-plugin-react-hooks eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-config-google eslint-config-kentcdodds eslint-config-xo eslint-plugin-eslint-comments eslint-plugin-json eslint-plugin-optimize-regex eslint-plugin-promise eslint-plugin-unicorn babel-plugin-module-resolver babel-plugin-react-require eslint-import-resolver-webpack eslint-import-resolver-babel-module && yarn audit'
+    'yarn add -D webpack prettier eslint-config-airbnb eslint-config-prettier eslint-plugin-babel eslint-plugin-flowtype eslint-plugin-html eslint-plugin-prettier eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-config-google eslint-config-kentcdodds eslint-config-xo eslint-plugin-eslint-comments eslint-plugin-json eslint-plugin-optimize-regex eslint-plugin-promise babel-plugin-module-resolver babel-plugin-react-require eslint-import-resolver-webpack eslint-import-resolver-babel-module eslint@latest babel-eslint@latest eslint-plugin-react-hooks@^1.7.0 && yarn audit'
   )
   console.log('thank you for installing eslint node modules <3!')
 })()
